af78ff732ef942e8c26cb09c50dd22936a687d4e
RedTengu <muratborokov@gmail.com>
2023-04-30 12:18:03 +0300

    fix: -

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,7 @@ module.exports = (req, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new Unauthorized('Необходима авторизация'));
+    return next(new Unauthorized('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
